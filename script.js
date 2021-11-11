1985052dfe68edefb20b16cd352b55245bd8b73d
thenamesjagger <67752390+thenamesjagger@users.noreply.github.com>
2021-11-10 16:33:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 // Change this to your username
 var username = "Jagger";
 
-if (localStorage.getItem('data') == null) var mode = "dark";
-else mode = localStorage.getItem('data');
-
 const determineGreet = hours => document.getElementById("greeting").innerText = `Good ${hours < 12 ? "Morning," : hours < 18 ? "Afternoon," : "Evening,"} ${username}.`;
 
-// The same as "onload"
 window.addEventListener('load', (event) => {
     let today = new Date();
     let time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
@@ -43,8 +39,8 @@ function displayTime(time) {
 function dark_colors() {
     let link = document.getElementsByTagName('a');
     let mode_id = document.getElementById("mode");
-    document.body.style.background = "#000000";
-    document.getElementById("greeting").style.color = "#dee3de";
+    document.body.style.background = "#2e3440";
+    document.getElementById("greeting").style.color = "#bf616a";
     mode_id.src = "ico/light.png";
     mode_id.style.filter = "invert(100%) sepia(0%) saturate(1620%) hue-rotate(8deg) brightness(94%) contrast(88%)";
     for (i = 0; i < link.length; i++) 
@@ -55,7 +51,7 @@ function dark_colors() {
 function light_colors() {
     let link = document.getElementsByTagName('a');
     let mode_id = document.getElementById("mode");
-    document.body.style.background = "#FFE8D4";
+    document.body.style.background = "#d8dee9";
     document.getElementById("greeting").style.color = "#767283";
     mode_id.src = "ico/dark.png";
     mode_id.style.filter = "invert(58%) sepia(12%) saturate(482%) hue-rotate(148deg) brightness(94%) contrast(86%)";
